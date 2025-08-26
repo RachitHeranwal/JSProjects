5871a8155882084a6cce0b93969c30c5b241e2d8
RachitHeranwal <frrachit@gmail.com>
2025-08-26 06:55:49 +0530

    Initial commit

---
+++ 01_todo_localStorage/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+
+let tasks =  JSON.parse(localStorage.getItem('tasks')) || [];
+
+tasks.forEach(task => {
+    renderTasks(task)
+});
+
+addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+
+    tasks.push(newTask);
+    saveTasks();
+    todoInput.value = ""; 
+    console.log(tasks);
+})
+
+function renderTasks(task) {
+    console.log(task);
+}
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+})
